58788e05b4800dc82851755f046f9dcd9cca3e92
Ravinesh Kumar <ravinesh.k@ergodeit.com>
2019-03-03 22:47:30 +0530

    Changes in Menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -172,7 +172,7 @@
                          <li class="nav-item dropdown dropdown-large">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Electronics <i class="fa fa-angle-down" aria-hidden="true"></i></a>
                                     
-                                    <!--<ul class="dropdown-menu dropdown-menu-large row">
+                                   <ul class="dropdown-menu dropdown-menu-large row">
                                         <li class="col-sm-3">
                                             <ul>
                                                 <li class="dropdown-header">Sword of Truths</li>
@@ -234,7 +234,7 @@
                                                 <li><img class"img-responsive" src="http://placehold.it/200x150"/></li>
                                             </ul>
                                         </li>
-                                    </ul>-->
+                                    </ul>
                           </li>
                           
                           <li class="nav-item dropdown dropdown-large">
