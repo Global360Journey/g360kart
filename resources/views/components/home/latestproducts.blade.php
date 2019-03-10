6a5c16b2bf25ea54e617ff7c7f2f2e1757f83083
ravinesh.kumar <ravinesh.k@ergodeit.com>
2019-03-11 01:31:40 +0530

    Changes in Product page

--- resources/views/components/home/latestproducts.blade.php
+++ resources/views/components/home/latestproducts.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="fillter_l_p_inner">
         <ul class="fillter_l_p">
-            <li class="active" data-filter="*"><a href="#">men's</a></li>
+            <li class="active" data-filter="*"><a href="#">Diary</a></li>
             <li data-filter=".woman"><a href="#">Woman</a></li>
             <li data-filter=".acc"><a href="#">Accessories</a></li>
             <li data-filter=".shoes"><a href="#">Shoes</a></li>
@@ -14,64 +14,65 @@
             <div class="col-lg-3 col-md-4 col-sm-6 woman bags">
                 <div class="l_product_item">
                     <div class="l_p_img">
-                        <img class="img-fluid" src="/images/img/product/l-product-1.jpg" alt="">
+                        <a href="https://www.amazon.in/pranjals-house-Notebook-Handmade-Notebooks/dp/B07M5XVP7Z/ref=as_li_ss_il?s=office&ie=UTF8&qid=1552243398&sr=1-29&keywords=Diary&linkCode=li3&tag=gohooked-21&linkId=c42e0538e528f66ce62407a2c7f57d8c&language=en_IN" target="_blank"><img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07M5XVP7Z&Format=_SL250_&ID=AsinImage&MarketPlace=IN&ServiceVersion=20070822&WS=1&tag=gohooked-21&language=en_IN" ><img src="https://ir-in.amazon-adsystem.com/e/ir?t=gohooked-21&language=en_IN&l=li3&o=31&a=B07M5XVP7Z" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /></a>
                     </div>
                     <div class="l_p_text">
                         <ul>
+                                
                             <li class="p_icon"><a href="#"><i class="icon_piechart"></i></a></li>
-                            <li><a class="add_cart_btn" href="#">Add To Cart</a></li>
+                            <li><a class="add_cart_btn" href="https://www.amazon.in/gp/product/B07DB4QDBY/ref=as_li_qf_asin_il_tl?ie=UTF8&tag=gohooked-21&creative=24630&linkCode=as2&creativeASIN=B07DB4QDBY&linkId=5ae3a74c02ca452c972cfcbf9657393e">Shop Now</a></li>
                             <li class="p_icon"><a href="#"><i class="icon_heart_alt"></i></a></li>
                         </ul>
-                        <h4>Womens Libero</h4>
-                        <h5><del>$45.50</del>  $40</h5>
+                        <h4>pranjals house Leather...</h4>
+                        <h5><del>INR 799.00 </del>  INR 499.00</h5>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6 acc shoes">
                 <div class="l_product_item">
                     <div class="l_p_img">
-                        <img class="img-fluid" src="/images/img/product/l-product-2.jpg" alt="">
+                            <a href="https://www.amazon.in/Doodle-Adventures-Mind-Notebook-Diary/dp/B01M22EOOX/ref=as_li_ss_il?s=office&ie=UTF8&qid=1552243590&sr=1-4&keywords=Diary&linkCode=li3&tag=gohooked-21&linkId=e47b73e3ee7a3a6c0590456770f4e8e2&language=en_IN" target="_blank"><img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01M22EOOX&Format=_SL250_&ID=AsinImage&MarketPlace=IN&ServiceVersion=20070822&WS=1&tag=gohooked-21&language=en_IN" ><img src="https://ir-in.amazon-adsystem.com/e/ir?t=gohooked-21&language=en_IN&l=li3&o=31&a=B01M22EOOX" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /></a>
                     </div>
                     <div class="l_p_text">
                         <ul>
                             <li class="p_icon"><a href="#"><i class="icon_piechart"></i></a></li>
-                            <li><a class="add_cart_btn" href="#">Add To Cart</a></li>
+                            <li><a class="add_cart_btn" href="https://amzn.to/2NVTRDr">Shop Now</a></li>
                             <li class="p_icon"><a href="#"><i class="icon_heart_alt"></i></a></li>
                         </ul>
-                        <h4>Womens Libero</h4>
-                        <h5><del>$45.50</del>  $40</h5>
+                        <h4>Doodle Adventures of...</h4>
+                        <h5><del>INR 499.00</del>  INR 345.00</h5>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6 woman shoes">
                 <div class="l_product_item">
                     <div class="l_p_img">
-                        <img class="img-fluid" src="/images/img/product/l-product-3.jpg" alt="">
+                            <a href="https://www.amazon.in/KRAFT-Handmade-Classic-Leather-Notebook/dp/B07DB48R7L/ref=as_li_ss_il?_encoding=UTF8&pd_rd_i=B07DB48R7L&pd_rd_r=d656e3f3-4364-11e9-a9f4-838997fed375&pd_rd_w=fuomD&pd_rd_wg=6XZQy&pf_rd_p=87667aae-831c-4952-ab47-0ae2a4d747da&pf_rd_r=S9VP1M2Z5VWBWVHQX2HA&psc=1&refRID=S9VP1M2Z5VWBWVHQX2HA&linkCode=li3&tag=gohooked-21&linkId=356a236f358b1fb83d3e3d39d644f48a&language=en_IN" target="_blank"><img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07DB48R7L&Format=_SL250_&ID=AsinImage&MarketPlace=IN&ServiceVersion=20070822&WS=1&tag=gohooked-21&language=en_IN" ></a><img src="https://ir-in.amazon-adsystem.com/e/ir?t=gohooked-21&language=en_IN&l=li3&o=31&a=B07DB48R7L" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
                     </div>
                     <div class="l_p_text">
                         <ul>
                             <li class="p_icon"><a href="#"><i class="icon_piechart"></i></a></li>
-                            <li><a class="add_cart_btn" href="#">Add To Cart</a></li>
+                            <li><a class="add_cart_btn" href="https://amzn.to/2Hc1q8C">Shop Now</a></li>
                             <li class="p_icon"><a href="#"><i class="icon_heart_alt"></i></a></li>
                         </ul>
-                        <h4>Womens Libero</h4>
-                        <h5><del>$45.50</del>  $40</h5>
+                        <h4>RjKart Handmade...</h4>
+                        <h5><del>INR 749.00 </del>  INR 499.00</h5>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6 woman shoes">
                 <div class="l_product_item">
                     <div class="l_p_img">
-                        <img class="img-fluid" src="/images/img/product/l-product-4.jpg" alt="">
+                            <a href="https://www.amazon.in/RjKart-Handmade-Designer-Leather-Sheets/dp/B07C7CHDZR/ref=as_li_ss_il?_encoding=UTF8&pd_rd_i=B07C7CHDZR&pd_rd_r=d656e3f3-4364-11e9-a9f4-838997fed375&pd_rd_w=fuomD&pd_rd_wg=6XZQy&pf_rd_p=87667aae-831c-4952-ab47-0ae2a4d747da&pf_rd_r=S9VP1M2Z5VWBWVHQX2HA&psc=1&refRID=S9VP1M2Z5VWBWVHQX2HA&linkCode=li3&tag=gohooked-21&linkId=e46353ca25dd6c3bb74141cfb5591ebe&language=en_IN" target="_blank"><img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07C7CHDZR&Format=_SL250_&ID=AsinImage&MarketPlace=IN&ServiceVersion=20070822&WS=1&tag=gohooked-21&language=en_IN" ><img src="https://ir-in.amazon-adsystem.com/e/ir?t=gohooked-21&language=en_IN&l=li3&o=31&a=B07C7CHDZR" width="1" height="250" border="0" alt="" style="border:none !important; margin:0px !important;" /></a>
                     </div>
                     <div class="l_p_text">
                         <ul>
                             <li class="p_icon"><a href="#"><i class="icon_piechart"></i></a></li>
-                            <li><a class="add_cart_btn" href="#">Add To Cart</a></li>
+                            <li><a class="add_cart_btn" target="_blank" href="https://amzn.to/2NUyZg2">Shop Now</a></li>
                             <li class="p_icon"><a href="#"><i class="icon_heart_alt"></i></a></li>
                         </ul>
-                        <h4>Womens Libero</h4>
-                        <h5><del>$45.50</del>  $40</h5>
+                        <h4>Di-Kraft Handmade Belt Lock ...</h4>
+                        <h5><del>INR 649.00  </del>  INR 449.00</h5>
                     </div>
                 </div>
             </div>
