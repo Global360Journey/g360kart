efc58c66ffddccd2082edbb15b782bc8f4e3d464
ravinesh.kumar <ravinesh.k@ergodeit.com>
2019-03-13 02:00:11 +0530

    Added New Menu Section

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -206,7 +206,7 @@
 <div class="carousel_menu_inner">
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/"><img src="/images/img/logo.png" alt=""></a>
+            <a class="navbar-brand" href="/">{!! setting('site.logo')  !!}<!--<img src="/images/img/logo.png" alt="">--></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
 
