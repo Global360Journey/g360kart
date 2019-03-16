6bd9001b8ed829f66dc893e14fdfb430f9a4701b
ravinesh.kumar <ravinesh.k@ergodeit.com>
2019-03-16 12:58:17 +0530

    Changes in banner section

--- resources/views/components/home/specialoffer.blade.php
+++ resources/views/components/home/specialoffer.blade.php
@@ -2,21 +2,17 @@
     <div class="row">
         <div class="col-lg-6">
             <div class="special_offer_item">
-                   
-                <img class="img-fluid" src="/images/img/feature-add/special-offer-1.jpg" alt="">
+                <a class="shop_now_btn" href="https://amzn.to/2UCIe7a" target="_blank"><img class="img-fluid" src="{{ Voyager::image(setting('banner.home-banner-left')) }}" alt=""></a>
                 <div class="hover_text">
-                    <h4>Special Offer</h4>
-                    <h5>Young Couple</h5>
-                    <a class="shop_now_btn" href="#">Shop Now</a>
                 </div>
             </div>
         </div>
         <div class="col-lg-6">
             <div class="special_offer_item2">
-                <img class="img-fluid" src="/images/img/feature-add/special-offer-2.jpg" alt="">
+                <img class="img-fluid1" src="{{ Voyager::image(setting('banner.home-banner-right')) }}" alt="" width="570" height="240">
                 <div class="hover_text">
-                    <h5>girls bag</h5>
-                    <a class="shop_now_btn" href="#">Shop Now</a>
+                    <h5>Home & Kitchen</h5>
+                    <a class="shop_now_btn" href="https://amzn.to/2FckuQC" target="_blank">Shop Now</a>
                 </div>
             </div>
         </div>
